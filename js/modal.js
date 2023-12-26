640d6027311f81d0e5c0f217add0266ebc6c4813
Lemon4ik94 <olegborbych@gmail.com>
2023-12-26 14:09:59 +0200

    Fix adaptive and make modal form

--- js/modal.js
+++ js/modal.js
@@ -1,8 +1,39 @@
-var token = '6926757237:AAF2PiXfLNSV53zfmrQanRjiUWvDm9fEWoM'
-var chat_id = '948384331';
-var subscribe = 'Just some text';
-var url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&text=${subscribe}&parse_mode=html`;
+(() => {
 
-let api = new XMLHttpRequest();
-api.open("GET", url, true);
-api.send();
+    const form = document.querySelector("#contact");
+    const refs = {
+        openModalBtn: document.querySelector("[data-modal-open]"),
+        closeModalBtn: document.querySelector("[data-modal-close]"),
+        modal: document.querySelector("[data-modal]"),
+    };
+
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+
+    function toggleModal() {
+        refs.modal.classList.toggle("is-hidden-modal");
+      }
+
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        var subscribe__email = document.getElementById("email").value;
+        var subscribe__name = document.getElementById("name").value;
+        var subscribe__phone = document.getElementById("phone").value;
+        var subscribe__comment = document.getElementById("comment").value;
+
+        var subscribe = `Користувач ${subscribe__name} хоче з вами зв'язатися.%0AПошта: ${subscribe__email}%0AТелефон: ${subscribe__phone}%0AКоментар: ${subscribe__comment}`
+
+        var token = '6926757237:AAF2PiXfLNSV53zfmrQanRjiUWvDm9fEWoM'
+        var chat_id = '948384331';
+        var url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&text=${subscribe}&parse_mode=html`;
+
+        let api = new XMLHttpRequest();
+        api.open("GET", url, true);
+        api.send();
+
+        form.reset()
+        toggleModal();
+    });
+})();
+
